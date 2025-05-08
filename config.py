62e37cca44535e16be967ec9f618000a928ee093
RRR-dot43 <ruhilrohit10@gmail.com>
2025-05-08 14:16:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-api_id =  28829496
-api_hash  = '8094341dbe5bd50090de8b12a01d04b8'
-bot_token =  '7819304857:AAFCdZWw8ElfIP4Se_kHo5qbdCexQNE6cT8'
-OWNER =  6722831365
-OWNER_USERNAME = 'Saksham'
+api_id =  21216779
+api_hash  = 'fdad88238287459c6dc790658f1008c1'
+bot_token =  '7322414204:AAFfoxprSqFzUkbgHL9vY88ewRQEPai_3LI'
+OWNER =  728093729
+OWNER_USERNAME = 'Rohit'
 LOG =  -100253268913
 ADMINS = [OWNER, 7161486, 181369839813]
 MONGO_URI = 'mongodb+srv://lodalesson876:FKxK7BZGq1WzE4ae@cluster0.or1gz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
